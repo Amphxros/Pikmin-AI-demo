1711054bf845271e8780f4d1f4aa6fd8c0ab503c
Amparo Rubio <amprubio@ucm.es>
2023-08-29 20:39:05 +0200

    Pikmin't

--- Assets/Scripts/Pikmin/Listen.cs
+++ Assets/Scripts/Pikmin/Listen.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PikminState : MonoBehaviour
+public class Listen : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
